503096e16bfeca3b17fec28543aa4cc512014866
PC Robotics <pcrobotics2@apps.district279.org>
2024-11-19 17:20:01 -0600

    Initial commit

---
+++ MeepMeepTesting/lib/src/main/java/com/example/lib/MeepMeepTesting.java
@@ -0,0 +1,33 @@
+package com.example.lib;
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+public class MeepMeepTesting {
+
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(800);
+
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(-60, -35, Math.toRadians(0)))
+                .setTangent(0)
+                .splineToLinearHeading(new Pose2d(-52, 53, Math.toRadians(137)), Math.PI)
+                .setTangent(Math.toRadians(270))
+                .splineToLinearHeading(new Pose2d(-60, -38, Math.toRadians(0)), Math.PI)
+                .build());
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_POWERPLAY_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedRightObservSquare.java
@@ -0,0 +1,156 @@
+package org.firstinspires.ftc.teamcode;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.config.Config;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+@Config
+@Autonomous(name = "RedRightObservSquare", group = "Autonomous")
+public class RedRightObservSquare extends LinearOpMode {
+
+    //global variables
+    public double liftPower = 0.1;
+    public double highChamberPos = 3000;
+    public double liftUpperLimit = 4000;
+    public double liftLowerLimit = 50;
+
+    // lift class
+    public class Lift {
+        public DcMotorEx lift;
+
+        public Lift(HardwareMap hardwareMap) {
+            lift = hardwareMap.get(DcMotorEx.class, "vertLinArm");
+            lift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+            lift.setDirection(DcMotorSimple.Direction.FORWARD);
+        }
+
+        public class LiftUp implements Action {
+            // checks if the lift motor has been powered on
+            private boolean initialized = false;
+
+            // actions are formatted via telemetry packets as below
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                // powers on motor, if it is not on
+                if (!initialized) {
+                    lift.setPower(liftPower);
+                    initialized = true;
+                }
+
+                // checks lift's current position
+                double pos = lift.getCurrentPosition();
+                packet.put("liftPos", pos);
+                if (pos < highChamberPos && pos < liftUpperLimit) {
+                    // true causes the action to rerun
+                    return true;
+                } else {
+                    // false stops action rerun
+                    lift.setPower(0);
+                    return false;
+                }
+                // overall, the action powers the lift until it surpasses
+                // 3000 encoder ticks, then powers it off
+            }
+        }
+
+        public Action liftUp() {
+            return new LiftUp();
+        }
+
+        public class LiftDown implements Action {
+            private boolean initialized = false;
+
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                if (!initialized) {
+                    lift.setPower(-liftPower);
+                    initialized = true;
+                }
+
+                double pos = lift.getCurrentPosition();
+                packet.put("liftPos", pos);
+                if (pos > 100.0 && pos > liftLowerLimit) {
+                    return true;
+                } else {
+                    lift.setPower(0);
+                    return false;
+                }
+            }
+        }
+
+        public Action liftDown() {
+            return new LiftDown();
+        }
+
+    }
+
+    // claw class
+    public static class Intake {
+        private final Servo intake;
+
+        public Intake(HardwareMap hardwareMap) {
+            intake = hardwareMap.get(Servo.class, "intake");
+        }
+
+        public class Reset implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                intake.setPosition(0.178);
+                return false;
+            }
+        }
+        public Action reset() {
+            return new Reset();
+        }
+
+        public class Release implements Action {
+            @Override
+            public boolean run(@NonNull TelemetryPacket packet) {
+                intake.setPosition(0.356);
+                return false;
+            }
+        }
+        public Action release() {
+            return new Release();
+        }
+
+    }
+
+    @Override
+    public void runOpMode() {
+
+        Pose2d initialPose = new Pose2d(60, 35, Math.toRadians(180));
+        MecanumDrive drive = new MecanumDrive(hardwareMap, initialPose);
+
+        TrajectoryActionBuilder tab1 = drive.actionBuilder(initialPose)
+                .setTangent(Math.toRadians(270))
+                .splineToConstantHeading(new Vector2d(38, 0), Math.toRadians(270))
+                .setTangent(Math.toRadians(270))
+                .splineToConstantHeading(new Vector2d(38, -50), Math.toRadians(270))
+                .setTangent(Math.toRadians(270))
+                .splineToConstantHeading(new Vector2d(60, -55), Math.toRadians(0));
+
+        if (isStopRequested()) return;
+
+        Actions.runBlocking(
+                new SequentialAction(
+                        tab1.build()
+                )
+        );
+    }
+
+}
